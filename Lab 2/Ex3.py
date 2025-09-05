1057a54c05ca815cfa0c57835823595050abe099
Jazian <jazian@hawaii.edu>
2025-09-05 12:13:25 -1000

    Changed Ex3.py back to original and added Ex4.py

--- Lab 2/Ex3.py
+++ Lab 2/Ex3.py
@@ -6,13 +6,7 @@
 print("Hi There")
 value_entered = input("Please enter an floating point number between 1 and 100: ")
 print("The user entered ", value_entered)
-
-# Convert the string to a floating (Allows use of decimal integers) point number
 vaule_as_float = float(value_entered)
 
-# Square the number
 value_squared = vaule_as_float ** 2
-
-# Round to 2 decimal places and print result
-value_rounded = round(value_squared, 2)
-print(f"The value squared is {value_rounded}")
+print(f"The value squared is {value_squared}")
---
+++ Lab 2/Ex4.py
@@ -0,0 +1,6 @@
+# Ask the user to enter a floating point number between 1 and 100.
+# Square the number and return the result rounded to 2 decimal places.
+# Name: Jazian Uejo
+# Date: 9/5/25
+
+value_entered = input()
