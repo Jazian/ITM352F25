ff1899236223f581f8e6d1ffae7d605e6aba188e
Jazian <jazian@hawaii.edu>
2025-09-05 12:39:18 -1000

    Completed Ex5.py and added Ex6.py

---
+++ Lab 2/Ex5.py
@@ -0,0 +1,9 @@
+# Ask the user to enter an arbitrary sentence. Calculate the legnth of the sentence and return the length of the user.
+# Name: Jazian Uejo
+# Date: 9/5/25
+
+user_sentence = input("Please enter a sentence: ")
+
+sentence_length = len(user_sentence)
+print("You entered: ", user_sentence)
+print(f"The length of the sentence is: {sentence_length}")
---
+++ Lab 2/Ex6.py
@@ -0,0 +1,7 @@
+# Ask the user to enter their weight in pounds. Convert the weight to kilograms and return the result to the user.
+# Name: Jazian Uejo
+# Date: 9/5/25
+
+weight_in_pounds = input("Please enter your weight in pounds: ")
+weight_in_pounds_float = float(weight_in_pounds) * 0.453592
+print(f"You weigh {weight_in_pounds_float} in kilograms")
