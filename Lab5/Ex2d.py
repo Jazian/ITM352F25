486f09a342984c79ad2c582f37f75219f1f0d067
Jazian <jazian@hawaii.edu>
2025-09-24 12:50:41 -1000

    Added Ex2 - 3

---
+++ Lab5/Ex2d.py
@@ -0,0 +1,11 @@
+# Create a list of dictionaries from the given dictionary
+# Name: Jazian Uejo
+# Date: 9/24/25
+
+trips = [ {'miles': 1.1, 'duration': '$6.25'},
+          {'miles': 0.8, 'duration': '$5.25'},
+          {'miles': 2.5, 'duration': '$10.50'},
+          {'miles': 2.6, 'duration': '$8.05'}
+         ]
+
+print(f"The first trip duration is {trips[0]['miles']} miles and cost is ${trips[0]['duration']:.2f}")
