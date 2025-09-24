5ab062da309acb6b5e5af42f6df43fcd700c3d4a
Jazian <jazian@hawaii.edu>
2025-09-24 13:15:58 -1000

    Added Lab 6 Ex 1

---
+++ Lab6/Ex1.py
@@ -0,0 +1,6 @@
+# Initialize a tuple with different emotions
+# Name: Jazian Uejo
+# Date: 9/24/25
+
+emotions = ("happy", "sad", "fear", "surprise")
+print(emotions[0] == "happy" and len(emotions) > 3)
---
+++ Lab6/Ex1b.py
@@ -0,0 +1,9 @@
+# Initialize a tuple with different emotions
+# Name: Jazian Uejo
+# Date: 9/24/25
+
+emotions = ("happy", "sad", "excited", "angry", "relaxed")
+if (len(emotions) > 3 and emotions[2] == "happy"):
+    print(True)
+else:
+    print("Len is not greater than 3 or someone is not happy")
