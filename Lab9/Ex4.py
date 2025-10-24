e71c71d90e7055f3ebf3abe96c716b77002d4786
Jazian <jazian@hawaii.edu>
2025-10-23 23:23:09 -1000

    Finished Lab 9

--- Lab9/Ex4.py
+++ Lab9/Ex4.py
@@ -1,5 +1,5 @@
-# Read the 1,000 lines of taxi data from the taxi_1000.csv file
-# Calculate he total of all fares, average fares, and the maximum trip distance
+# Modify the code from Ex3.py to calculate the total of all fares, the average of those fares, and the maximum trip distance (based on the Trip Miles field) for records that have fares greater than 10 dollars.
+
 
 import csv
 
@@ -21,10 +21,15 @@ with open("taxi_1000.csv", "r") as csv_file:
                 max_distance = distance
         num_rows += 1
 
+    if tripfare > 10:
+        total_fares += tripfare
+        max_distance = max(max_distance, distance)
+        num_rows += 1
+
     if num_rows > 0:
         average_fare = round(total_fares / num_rows, 2)
 
-    print(f"We read {num_rows} rows")
+    print(f"Rows with fares greater than $10: {num_rows}")
     print(f"Total Fares: ${total_fares:,.2f}")
     print(f"Average Fare: ${average_fare:,.2f}")
     print(f"Maximum Distance: {max_distance:,.2f}")
